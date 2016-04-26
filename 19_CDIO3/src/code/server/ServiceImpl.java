6bf41974eff85ce3bd0a73473145442c04419a4c
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-04-26 09:31:15 +0200

    fået implementeret et interface til server servicen

--- 19_CDIO3/src/code/server/ServiceImpl.java
+++ 19_CDIO3/src/code/server/ServiceImpl.java
@@ -2,9 +2,35 @@ package code.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class ServiceImpl extends RemoteServiceServlet {
+import code.client.service.IService;
+
+public class ServiceImpl extends RemoteServiceServlet implements IService {
 
 	public ServiceImpl() {
+		
+	}
+
+	@Override
+	public void createUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deactivateUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void activateUser() {
+		// TODO Auto-generated method stub
 		
 	}
 	
